960bcdb1d5b700daba5dee5d17feffc6c4290f09
ynwc2006 <58402928+ynwc2006@users.noreply.github.com>
2021-03-26 21:04:23 -0400

    section 53

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -7,7 +7,7 @@ const {isLoggedIn, isAuthor,validateCampground}=require('../middleware');
 
 
 
-router.get('/', catchAsync(campgrounds.index));
+/* router.get('/', catchAsync(campgrounds.index));
 
 //create new
 router.get('/new', isLoggedIn,campgrounds.renderNewForm)
@@ -24,5 +24,28 @@ router.put('/:id',isLoggedIn,isAuthor, validateCampground,catchAsync(campgrounds
 
 //delete
 router.delete('/:id',isLoggedIn,isAuthor,catchAsync(campgrounds.deleteCampground))
+ */
+
+
+router.route('/')
+    .get( catchAsync(campgrounds.index))
+    .post( isLoggedIn,validateCampground,catchAsync(campgrounds.createCampground))
+
+
+router.get('/new', isLoggedIn,campgrounds.renderNewForm)
+
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn,isAuthor, validateCampground,catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn,isAuthor,catchAsync(campgrounds.deleteCampground))
+
+
+//update
+router.get('/:id/edit',isLoggedIn,isAuthor, catchAsync(campgrounds.renderEditForm))
+
+
+
+
+
 
 module.exports=router;
